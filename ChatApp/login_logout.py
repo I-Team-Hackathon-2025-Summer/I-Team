f830dad5423966140f34dc46007262b0473e2156
satomoemi <meiyin6888@gmail.com>
2025-07-26 19:23:47 +0900

    init.sql更新

---
+++ ChatApp/login_logout.py
@@ -0,0 +1,14 @@
+from flask import Frask, request, redirect, render_template, session, flash,url_for
+
+app = Flask(__name__)
+
+#ログインページ
+@app.route('/login', methods=['GET'])
+def login_view():
+    return render_template('auth/login.html')
+
+#ログイン処理
+@app.route('/login', methods=['POST'])
+def login_process():
+    email = request.form.get('email')
+    password = request.form.get('password')
