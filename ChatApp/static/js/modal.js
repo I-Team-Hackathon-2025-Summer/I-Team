979cbaa7bbcb2c762c0c574bd3be2be342a8c6b9
Yanko <kurata.yuca@gmail.com>
2025-08-24 03:23:43 +0900

    モーダル

--- ChatApp/static/js/modal.js
+++ ChatApp/static/js/modal.js
@@ -1,18 +1,21 @@
 
-const openModal = document.getElementById("openModal");
+const openBtns = document.querySelectorAll(".openModal");
 const modal = document.getElementById("modalDelete");
 const deleteBtn = document.getElementById("delete");
 const cancelBtn = document.getElementById("cancel");
 
 // モーダルを開く
-openModal.addEventListener('click', () => {
-  modal.showModal();   //モーダルを表示
+openBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    modal.style.display = "flex"; // モーダルを表示
+  });
 });
+
 
 // 削除ボタンをクリックしたとき
 deleteBtn.addEventListener('click', () => {
   //削除処理を記述する
-    console.log("メッセージを削除しました");
+    alert("メッセージを削除しました");
   modal.style.display = "none"; // モーダルを閉じる
 });
 
@@ -22,8 +25,8 @@ cancelBtn.addEventListener('click', () => {
 });
 
 // モーダル外（背景）をクリックしたら閉じる
-confirmDialog.addEventListener('click', (e) => {
-    if (e.target === confirmDialog) {
-        confirmDialog.close();
+addEventListener('click', (e) => {
+    if (e.target === modal) {
+      modal.style.display = "none"; // モーダルを閉じる
     }
 });
