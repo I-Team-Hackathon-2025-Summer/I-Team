ca985586883e88650ec2c133a933cf80a911500b
Yanko <kurata.yuca@gmail.com>
2025-08-24 03:52:10 +0900

    モーダル

--- ChatApp/static/js/modal.js
+++ ChatApp/static/js/modal.js
@@ -1,20 +1,23 @@
 
 const openBtns = document.querySelectorAll(".openModal");
+// querySelectorAllで、class=openModalのボタンをすべて取得して
+// openBtns という配列のようなリストに入れる
 const modal = document.getElementById("modalDelete");
 const deleteBtn = document.getElementById("delete");
 const cancelBtn = document.getElementById("cancel");
 
 // モーダルを開く
 openBtns.forEach((btn) => {
+  // forEachはリストから1つずつ取り出して実行するforループのような処理
   btn.addEventListener("click", () => {
-    modal.style.display = "flex"; // モーダルを表示
+    modal.style.display = "flex"; // モーダルを表示する
   });
 });
 
 
 // 削除ボタンをクリックしたとき
 deleteBtn.addEventListener('click', () => {
-  //削除処理を記述する
+  //ここに削除処理を記述する
     alert("メッセージを削除しました");
   modal.style.display = "none"; // モーダルを閉じる
 });
@@ -26,7 +29,10 @@ cancelBtn.addEventListener('click', () => {
 
 // モーダル外（背景）をクリックしたら閉じる
 addEventListener('click', (e) => {
+  // (e)は "click"というイベントに関する情報（イベントオブジェクト）をaddEventListenerから渡される
+  // どこをクリックしたか、要素はなにか、などの情報が(e)に入る
     if (e.target === modal) {
-      modal.style.display = "none"; // モーダルを閉じる
+      // クリックされた要素(e.target)が modal（半透明部分） なら閉じる
+      modal.style.display = "none";
     }
 });
