fbaa8667e2902b068b552b7c69f0c67f8f0e3983
satomoemi <meiyin6888@gmail.com>
2025-08-28 19:11:15 +0900

    戻るボタンのための実装

--- ChatApp/views/chat.py
+++ ChatApp/views/chat.py
@@ -8,8 +8,10 @@ chat = Blueprint('chat', __name__, template_folder = 'templates', static_folder 
 
 
 #チャット画面
-@chat.route('/channels/<channel_id>/chat', methods=['GET'])
-def chat_view(channel_id):
+#/<area_id>がないと前のチャンネル画面に戻れない
+@chat.route('/<area_id>/channels/<channel_id>/chat', methods=['GET'])
+#area_idを引数として渡さないとエラーになる(URLで来たarea_idを受け取る場所が必要なのだと思う)
+def chat_view(area_id, channel_id):
   user_id = session.get('user_id')
   if user_id is None:
     return redirect(url_for('login_logout.login_view'))
@@ -19,11 +21,12 @@ def chat_view(channel_id):
     #メッセージとユーザ名表示
     messages = Chat.messages_get_all(channel_id)
     
-    return render_template('chat/chat.html', channels=channels, messages=messages, user_id=user_id, page='chat')
+    return render_template('chat/chat.html',  channels=channels, messages=messages, user_id=user_id, page='chat')
 
 #メッセージ作成
-@chat.route('/channels/<channel_id>/chat', methods=['POST'])
-def create_messages(channel_id):
+@chat.route('/<area_id>/channels/<channel_id>/chat', methods=['POST'])
+#area_idを引数として渡さないとエラーになる(URLで来たarea_idを受け取る場所が必要なのだと思う)
+def create_messages(area_id,channel_id):
   user_id = session.get('user_id')
   if user_id is None:
     return redirect(url_for('login_logout.login_view'))
@@ -31,16 +34,16 @@ def create_messages(channel_id):
   message = request.form.get('message')
   if message:
       Chat.create(user_id, channel_id, message)
-  return redirect(url_for('chat.chat_view', channel_id=channel_id))
+  return redirect(url_for('chat.chat_view', area_id=area_id, channel_id=channel_id))
 
 #メッセージ削除
-#urlにchannel_idがないとurl_forのリダイレクト先に戻れない(/channels/<channel_id>/chat)
-@chat.route('/<channel_id>/chat/delete_message/<message_id>', methods=['POST'])
-def delete_messages(channel_id, message_id):
+#urlにarea_idとchannel_idがないとurl_forのリダイレクト先に戻れない(/<area_id>/channels/<channel_id>/chat)
+@chat.route('/<area_id>/<channel_id>/chat/delete_message/<message_id>', methods=['POST'])
+def delete_messages(area_id, channel_id, message_id):
   user_id = session.get('user_id')
   if user_id is None:
     return redirect(url_for('login_logout.login_view'))
-  
+
   if message_id:
     Chat.delete_message(message_id)
-    return redirect(url_for('chat.chat_view', channel_id=channel_id))
+    return redirect(url_for('chat.chat_view', area_id=area_id, channel_id=channel_id))
